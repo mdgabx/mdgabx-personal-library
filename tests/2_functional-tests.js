64c64e3901ed693a83621bf496b5ee833a3de066
Mark Dexter Abuyabor <a.markdexter@gmail.com>
2023-12-01 20:27:33 +0800

    add: test case for GET /api/books/[id]

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -89,7 +89,17 @@ suite('Functional Tests', function() {
     suite('GET /api/books/[id] => book object with [id]', function(){
       
       test('Test GET /api/books/[id] with id not in db',  function(done){
-        done();
+        chai.request(server)
+            .get('/api/book')
+            .send({ id: '1234' })
+            .end(function (err, res) {
+              assert.equal(res.status, 404)
+              assert.isEmpty(res.body, "Test if there is data")
+
+              done();
+            })
+
+
       }).timeout(10000)
       
       test('Test GET /api/books/[id] with valid id in db',  function(done){
